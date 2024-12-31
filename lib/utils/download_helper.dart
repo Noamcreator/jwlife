bf1de78161e2a8df5927be44027523b70657221a
Noam <Noam.bourmault@gmail.com>
2024-12-31 04:21:47 +0100

    DocumentView Update, BottomNavigationBar Portrait And Landscape.

--- lib/utils/download_helper.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:convert';
-import 'dart:io';
-
-Future<void> downloadFileFromUrlToFile(String body, File file) async {
-  var bytes = utf8.encode(body);
-  await file.writeAsBytes(bytes);
-}
